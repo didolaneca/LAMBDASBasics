d2025393591e063e8f518ddf4c74ff6b020bf116
Diyan Simeonov <didolaneca@yahoo.com>
2019-10-06 12:25:04 -0500

    Added Greeter method.

---
+++ src/com/Diyan/Greeter.java
@@ -0,0 +1,12 @@
+package com.Diyan;
+
+public class Greeter {
+    public void greet(){
+        System.out.println("Hello Diyan!");
+    }
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.greet();
+    }
+}
