23d3f0e5c1a0f2238da2a117454f2a964f011dc7
Diyan Simeonov <didolaneca@yahoo.com>
2020-01-18 11:43:23 -0600

    Reworked JAVA8 approach with Predicate.

--- src/com/diyan/exercise/ExerciseOneJava8.java
+++ src/com/diyan/exercise/ExerciseOneJava8.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ExerciseOneJava8 {
     public static void main(String[] args) {
@@ -22,9 +23,9 @@ public class ExerciseOneJava8 {
 //        Step 3 Create a method that prints all people that have last name begining with S
         printConditionally(people, p -> p.getLastName().startsWith("S"));
     }
-    private static void printConditionally(List<Person> people, Condition condition) {
+    private static void printConditionally(List<Person> people, Predicate<Person> predicate) {
         for (Person p: people) {
-            if(condition.test(p)){
+            if(predicate.test(p)){
                 System.out.println(p);
             }
         }
